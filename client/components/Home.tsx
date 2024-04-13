dc63f49ad12a8641a8030ad4613a09848c1c2f4f
Jack Crawford <primes_unit.05@icloud.com>
2024-04-14 01:33:29 +1200

    fixed image path

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,32 +1,15 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import WidgetForecast from './WidgetForecast'
 import Toggle from './Toggle'
 import WeatherCurrent from './WeatherCurrent'
 
-const desktopImage = '/images/a3.png'
-const mobileImage = '/images/a3.png'
-
 function Home() {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
   const [backgroundColor, setBackgroundColor] = useState('bg-blue-300')
-  const imageUrl = windowWidth >= 850 ? desktopImage : mobileImage
-
-  useEffect(() => {
-    const handleWindowResize = () => {
-      setWindowWidth(window.innerWidth)
-    }
-
-    window.addEventListener('resize', handleWindowResize)
-
-    return () => {
-      window.removeEventListener('resize', handleWindowResize)
-    }
-  }, [])
 
   return (
     <>
       <div
-        className={`m-5 ${backgroundColor} bg-[url('${imageUrl}')] bg-contain bg-top bg-no-repeat p-10`}
+        className={`m-5 ${backgroundColor} bg-[url('/images/a3.png')] bg-contain bg-top bg-no-repeat p-10`}
       >
         <Toggle setBackgroundColor={setBackgroundColor} />
         <WeatherCurrent />
--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,15 +1,15 @@
-import { Outlet } from 'react-router-dom'
+// import { Outlet } from 'react-router-dom'
 
-export default function Layout() {
-  return (
-    <>
-      <header>
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-      </header>
-      <main>
-        <Outlet />
-      </main>
-      <footer></footer>
-    </>
-  )
-}
+// export default function Layout() {
+//   return (
+//     <>
+//       <header>
+//         <h1>Weather API</h1>
+//       </header>
+//       <main>
+//         <Outlet />
+//       </main>
+//       <footer></footer>
+//     </>
+//   )
+// }
